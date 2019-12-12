76aa06c744d5b5fc9fbd485de155ecdfe76ad0c0
jamniir <jami.niiranen@student.laurea.fi>
2019-12-12 13:57:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 $(function()
 {
-  $("#lista").keydown(function(evt) {
-    if (evt.which == 13) {
-      var aktiviteetti = $("#lista").val();
-      if (aktiviteetti.length < 2) { return false; }
-      var html = "<li><span class='poista'>" + aktiviteetti + "</span></li>";
-      $(html).insertAfter("#lista");
-      $("#lista").val("");
+  $("#lista").keydown(function(evt) { // Functio kun nappia painaa
+    if (evt.which == 13) { // Nappi nr. 13 = enter
+      var aktiviteetti = $("#lista").val(); // Input kentän arvo aktiviteetti-muuttujaan
+      if (aktiviteetti.length < 2) { return false; } // Jos 0-2 kirjainta, mitään ei tapahdu
+      var html = "<li><span class='poista'>" + aktiviteetti + "</span></li>"; // Muuttujaan html tehdään listaus missä aktiviteetti mukana
+      $(html).insertAfter("#lista"); // Lisätään edellä tehty input-kentän perään
+      $("#lista").val(""); // Nollataan arvo
     }
   });
 
-  $(document).on('click', '.poista', function()
+  $(document).on('click', '.poista', function() // Poisto klikkaamalla
     {
       $(this).remove();
     });
