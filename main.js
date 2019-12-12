e7981afd59e0912bc7d520bdd5c30d608fe65059
jamniir <jami.niiranen@student.laurea.fi>
2019-12-12 13:49:03 +0200

    Filut

---
+++ main.js
@@ -0,0 +1,17 @@
+$(function()
+{
+  $("#lista").keyup(function(evt) {
+    if (evt.which == 13) {
+      var aktiviteetti = $("#lista").val();
+      if (aktiviteetti.length < 2) { return false; }
+      var html = "<li><span class='poista'>" + aktiviteetti + "</span></li>";
+      $(html).insertAfter("#lista");
+      $("#lista").val("");
+    }
+  });
+
+  $(document).on('click', '.poista', function()
+    {
+      $(this).remove();
+    });
+});
